3943f59297dd058f5fa2f674dbdcc4805f804985
理想 <maming.zhong@tongdun.cn>
2017-10-03 16:59:02 +0800

    add other function interface

---
+++ jdk8/src/main/java/com/exmyth/beyond/jdk8/SupplierTest.java
@@ -0,0 +1,12 @@
+package com.exmyth.beyond.jdk8;
+
+
+import java.util.function.Supplier;
+
+public class SupplierTest {
+
+    public static void main(String[] args) {
+        Supplier<String> supplier = () -> "hello world";
+        System.out.println(supplier.get());
+    }
+}
