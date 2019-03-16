38896a10a02f6ef7a705e646432fa6df350d794f
理想 <maming.zhong@tongdun.cn>
2019-03-16 17:17:14 +0800

    auto increasing

--- DataStructure/src/com/exmyth/beyond/hello/datastructure/array/Array.java
+++ DataStructure/src/com/exmyth/beyond/hello/datastructure/array/Array.java
@@ -26,11 +26,12 @@ public class Array<E> {
     }
 
     public void add (int index, E e){
-        if(size == data.length){
-            throw new IndexOutOfBoundsException();
-        }
         if(index < 0 || index > size){
             throw new IllegalArgumentException();
+        }
+
+        if(size == data.length){
+            resize(2 * data.length);
         }
         for (int i = size - 1; i >= index; i--) {
             data[i+1] = data[i];
@@ -38,6 +39,15 @@ public class Array<E> {
         data[index] = e;
         size++;
     }
+
+    private void resize(int newCapacity) {
+        E[] newData = (E[]) new Object[newCapacity];
+        for (int i = 0; i < size; i++) {
+            newData[i] = data[i];
+        }
+        data = newData;
+    }
+
     public void addLast(E e){
         add(size, e);
     }
@@ -88,6 +98,9 @@ public class Array<E> {
         }
         size--;
         data[size] = null;//loitering object != memory leak
+        if(size == data.length /2){
+            resize(data.length/2);
+        }
         return result;
     }
 
--- DataStructure/src/com/exmyth/beyond/hello/datastructure/array/ArrayClient.java
+++ DataStructure/src/com/exmyth/beyond/hello/datastructure/array/ArrayClient.java
@@ -2,7 +2,7 @@ package com.exmyth.beyond.hello.datastructure.array;
 
 public class ArrayClient {
     public static void main(String[] args) {
-        Array<Integer> array = new Array<>(20);
+        Array<Integer> array = new Array<>();
         for (int i = 0; i < 10; i++) {
             array.addLast(i);
         }
